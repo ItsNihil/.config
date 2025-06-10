e102cf2ccf7504a550d60f1554cf6bd7a6951cd9
ItsNihil <166008023+ItsNihil@users.noreply.github.com>
2025-06-10 00:04:53 -0700

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,17 +1,19 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-config.font = wezterm.font 'JetBrains Mono'
--- For example, changing the color scheme:
-config.color_scheme = 'Dark Pastel'
+config.enable_wayland = false
 
+config.color_scheme = 'Dark Pastel'
+config.font = wezterm.font 'JetBrains Mono Nerd Font'
 config.font_size = 12
 
+config.max_fps = 180
+config.animation_fps = 30
 config.window_background_opacity = 0.8
 
--- and finally, return the configuration to wezterm
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.tab_and_split_indices_are_zero_based = false
+
 return config
