60fc6f232e4e225919e6ce4530b9819cb0f36876
ItsNihil <166008023+ItsNihil@users.noreply.github.com>
2025-06-11 17:34:59 -0700

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,19 +1,48 @@
 local wezterm = require 'wezterm'
-
+local mux = wezterm.mux
+local act = wezterm.action
 local config = wezterm.config_builder()
-
+-- Wayland Support
 config.enable_wayland = false
-
+-- Colors and Fonts
 config.color_scheme = 'Dark Pastel'
 config.font = wezterm.font 'JetBrains Mono Nerd Font'
 config.font_size = 12
-
-config.max_fps = 180
+-- Animations and Opacity
+config.max_fps = 60
 config.animation_fps = 30
 config.window_background_opacity = 0.8
-
+-- Tabs
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 config.tab_and_split_indices_are_zero_based = false
+-- Keybinds
+config.leader = {
+  key = 'a',
+  mods = 'CTRL',
+  timeout_milliseconds = 2000,
+}
+config.keys = {
+  {
+    key = 'c',
+    mods = 'LEADER',
+    action = act.SpawnTab 'CurrentPaneDomain',
+  },
+  {
+    key = 'n',
+    mods = 'LEADER',
+    action = act.ActivateTabRelative(1),
+  },
+  {
+    key = 'p',
+    mods = 'LEADER',
+    action = act.ActivateTabRelative(-1),
+  },
+  {
+    key = 'w',
+    mods = 'LEADER',
+    action = act.CloseCurrentTab{confirm = false},
+  },
+}
 
 return config
